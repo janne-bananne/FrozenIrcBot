347227d32dda68cff021fedcb9d49785d654723f
Janne Koschinski <janne@kuschku.de>
2014-10-04 15:45:16 +0200

    Added plugin functionality
    Will now load plugin classes from a plugins/ folder.

--- src/main/java/de/kuschku/ircbot/Client.java
+++ src/main/java/de/kuschku/ircbot/Client.java
@@ -2,7 +2,11 @@ package de.kuschku.ircbot;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.lang.reflect.InvocationTargetException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLClassLoader;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -16,7 +20,7 @@ import org.pircbotx.hooks.Listener;
 import de.kuschku.ircbot.handlers.QuakeNetLoginHandler;
 
 public class Client {
-	
+
 	public static FileConfiguration fileConfiguration;
 	private PircBotX bot;
 
@@ -32,39 +36,45 @@ public class Client {
 	}
 
 	public Client(Options options) {
-		Logger.getLogger(Client.class.getCanonicalName()).setLevel(Level.WARNING);
-		
 		try {
-			fileConfiguration = FileConfiguration.fromFile(new File(options.configpath));
+			fileConfiguration = FileConfiguration.fromFile(new File(
+					options.configpath));
 		} catch (FileNotFoundException e1) {
 			e1.printStackTrace();
 		}
-		
+
 		// Setup this bot
 		Configuration.Builder<PircBotX> builder = new Configuration.Builder<PircBotX>()
 				.setName(fileConfiguration.get("name"))
 				.setLogin(fileConfiguration.get("auth_name"))
 				.setAutoNickChange(false).setCapEnabled(true)
 				.setServerHostname(fileConfiguration.get("hostname"));
-		
+
 		switch (fileConfiguration.get("auth_type")) {
 		case "NickServ":
 			builder.setNickservPassword(fileConfiguration.get("auth_password"));
 			break;
 		case "TheQBot":
-			enableHandler(builder,QuakeNetLoginHandler.class.getCanonicalName());
+			enableHandler(builder,
+					QuakeNetLoginHandler.class.getCanonicalName());
 			break;
 		default:
 			break;
 		}
-		
+
 		for (String channel : fileConfiguration.get("channel").split(",")) {
 			builder.addAutoJoinChannel(channel);
 		}
 
 		for (String handler : fileConfiguration.get("handler").split(",")) {
-			enableHandler(builder,handler);
+			enableHandler(builder, handler);
 		}
+
+		File[] paths = new File[options.pluginpath.split(",").length];
+		for (int i = 0; i<options.pluginpath.split(",").length; i++) {
+			paths[i] = new File(options.pluginpath.split(",")[i]);
+		}
+		loadPlugins(builder, paths);
 
 		this.bot = new PircBotX(builder.buildConfiguration());
 
@@ -73,24 +83,29 @@ public class Client {
 		} catch (Exception ex) {
 		}
 	}
-	
+
 	@SuppressWarnings("unchecked")
-	public static void enableHandler(Configuration.Builder<PircBotX> builder, String handler) {
+	public static void enableHandler(Configuration.Builder<PircBotX> builder,
+			String handler) {
 		try {
 			builder.addListener((Listener<PircBotX>) newInstance(handler));
-			Client.log(Level.INFO,"Successfully loaded handler "+handler);
+			Client.log(Level.INFO, "Successfully loaded handler " + handler);
 		} catch (ClassNotFoundException | NoSuchMethodException
 				| InstantiationException | IllegalAccessException
 				| IllegalArgumentException | InvocationTargetException e) {
-			Client.log(Level.WARNING,"Handler could not be activated: " + handler);
+			Client.log(Level.WARNING, "Handler could not be activated: "
+					+ handler);
 		}
 	}
 
 	public static class Options {
 		@Option(name = "-config")
 		private String configpath = "config.yml";
+
+		@Option(name = "-plugins")
+		private String pluginpath = "plugins";
 	}
-	
+
 	public static void log(Level level, String message) {
 		Logger.getLogger(Client.class.getCanonicalName()).log(level, message);
 	}
@@ -108,5 +123,60 @@ public class Client {
 		}
 		return (T) Class.forName(className).getConstructor(types)
 				.newInstance(args);
+	}
+
+	@SuppressWarnings("unchecked")
+	public static <T> T loadPlugin(URL path, String classname)
+			throws ClassNotFoundException, InstantiationException,
+			IllegalAccessException, IllegalArgumentException,
+			InvocationTargetException, NoSuchMethodException,
+			SecurityException, IOException {
+		try (URLClassLoader ucl = new URLClassLoader(new URL[] { path })) {
+			ucl.loadClass(classname);
+			return (T) Class.forName(classname, true, ucl)
+					.getConstructor(new Class[0]).newInstance(new Object[0]);
+		}
+	}
+
+	public static void loadPlugins(Configuration.Builder<PircBotX> builder,
+			File[] paths) {
+		for (File path : paths) {
+			if (path.exists() && path.isDirectory()) {
+				for (File file : path.listFiles()) {
+					if (file.isFile() && file.getName().endsWith(".class")) {
+						String className = file.getName().substring(0,
+								file.getName().lastIndexOf("."));
+						try {
+							builder.addListener(loadPlugin(path
+									.getAbsoluteFile().toURI().toURL(),
+									className));
+							Client.log(Level.INFO, "Plugin loaded: "
+									+ path.getAbsoluteFile().toURI().toURL()
+									+ " : " + className);
+						} catch (ClassNotFoundException
+								| InstantiationException
+								| IllegalAccessException
+								| IllegalArgumentException
+								| InvocationTargetException
+								| NoSuchMethodException | SecurityException
+								| IOException e) {
+							try {
+								Client.log(Level.WARNING,
+										"Could not load plugin: "
+												+ path.getAbsoluteFile()
+														.toURI().toURL()
+												+ " : " + className);
+							} catch (MalformedURLException e1) {
+								e1.printStackTrace();
+							}
+							e.printStackTrace();
+						}
+					}
+				}
+			} else if (path.exists() && path.getName().endsWith(".jar")){
+			} else {
+				path.mkdirs();
+			}
+		}
 	}
 }
